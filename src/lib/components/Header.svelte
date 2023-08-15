7d37596baec68674dc59a075e50f147216a5abe4
Robinho1997 <robin.kempen@gmx.de>
2023-08-15 03:24:18 +0200

    created components folder and a Header Component and imported Header into the Layout

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,17 @@
+<header>
+    <a href="/">Home</a>
+
+    <nav>
+        <ul>
+            <li>
+                <a href="/blog">Blog</a>
+            </li>
+            <li>
+                <a href="/über-mich">Über mich</a>
+            </li>
+            <li>
+                <a href="/kontakt">Kontakt</a>
+            </li>
+        </ul>
+    </nav>
+</header>
