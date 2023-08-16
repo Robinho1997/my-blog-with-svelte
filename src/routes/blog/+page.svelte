019e93a00f78292f53dd44e3e29f8e74cf69ba0e
Robinho1997 <robin.kempen@gmx.de>
2023-08-16 02:39:48 +0200

    created endpoint for the blogposts and a utility to fetch posts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,3 +1,18 @@
-<h1>Blog</h1>
-
-<p>Meine Blog post werden hier sein irgendwann..</p>
+<script>
+    export let data
+    </script>
+    
+    <h1>Blog</h1>
+      
+    <ul>
+      {#each data.posts as post}
+        <li>
+          <h2>
+            <a href={post.path}>
+              {post.meta.title}
+            </a>
+          </h2>
+          Published {post.meta.date}
+        </li>
+      {/each}
+    </ul>
