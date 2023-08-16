ba655b1c9776857274b2e38de6553e2293197182
Robinho1997 <robin.kempen@gmx.de>
2023-08-16 02:43:41 +0200

    add static adapter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
     import Header from "../lib/components/Header.svelte";
     import Footer from "../lib/components/Footer.svelte";
-    import "$lib/styles/style.scss"
+    import "$lib/styles/style.scss";
+
 </script>
 
 <Header />
